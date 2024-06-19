6b807bda93e22a99f7337651bc84e377ca72a62d
- <ramirez.ares@correo.unimet.edu.ve>
2024-06-19 13:37:45 -0400

    Avance 2. Componente NavBar y Footer

--- metroeats-granier/src/components/ChangingNavBar.jsx
+++ metroeats-granier/src/components/ChangingNavBar.jsx
@@ -3,34 +3,70 @@ import "./ChangingNavBar.style.css";
 import ChangingButton from "./ChangingButton";
 import ProfileButton from "./ProfileButton";
 import Img from "./Img";
+import { useState, useEffect } from "react";
 
 const ChangingNavBar = (props) => {
-  const kind = props.kind;
 
-  if (kind === "offline") {
+  const [state, setState] = useState("offline");
+
+  useEffect(() => {
+    setState(props.navstate);
+  }, [props.navstate]);
+
+  //Offline
+  if (state === "offline") {
     return (
       <div className="navbar">
         <Img route="/logo-provicional-unimet-metroeats.jpg" alt="Img" />
-        Desconectado 
-        <ChangingButton text="Regístrate" style="bordered-blue-background" link="" />
-        <ChangingButton text="Inicia Sesion" style="bordered-blue-background" link="" />
+        Desconectado
+        <ChangingButton
+          text="Regístrate"
+          style="bordered-blue-background"
+          link="https://refactoring.guru/es/design-patterns/chain-of-responsibility"
+        />
+        <ChangingButton
+          text="Inicia Sesion"
+          style="bordered-blue-background"
+          link="https://refactoring.guru/es/design-patterns/chain-of-responsibility"
+        />
       </div>
     );
-  } else if (kind === "client") {
+
+  //User  //TODO Hacer boton carrito button imag
+  } else if (state === "user") {
     return (
       <div className="navbar">
         <Img route="/logo-provicional-unimet-metroeats.jpg" alt="Img" />
-        Cliente
-        <ProfileButton text="Ares" style="borderless" link=""/>
+        <Img route="/logo-provicional-unimet-metroeats.jpg" alt="Img" />
+        User
+        <ChangingButton text="Menú" style="bordered-blue-background" link="" />
+        <ChangingButton text="Conócenos" style="bordered-blue-background" link="" />
+        
+        <ProfileButton text="Ares" style="borderless" link="" />
       </div>
     );
-  } else {
+
+  //Granier
+  } else if (state === "granier") {
+    return (
+      <div className="navbar">
+        Granier
+        <ChangingButton text="A" style="bordered-blue-background" link="" />
+        <ProfileButton text="Ares" style="borderless" link="" />
+      </div>
+    );
+  
+  //Employee
+  } else if (state === "employee") {
     return (
       <div className="navbar">
         Empleado
         <ChangingButton text="A" style="bordered-blue-background" link="" />
       </div>
     );
+  }
+  else {
+    return <div>Error: state no es 0. El valor actual de state es {state}.</div>;
   }
 };
 
